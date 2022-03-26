2d219d89ce44be8212b3b9e118427cad641ceb95
zhangwei <zhangwei24@apache.org>
2022-03-26 11:07:51 +0800

    test

--- functions-framework/golang/Knative/http/userfunction/hello.go
+++ functions-framework/golang/Knative/http/userfunction/hello.go
@@ -2,10 +2,43 @@ package userfunction
 
 import (
 	"fmt"
+	"io/ioutil"
 	"net/http"
+
+	"github.com/SkyAPM/go2sky"
+	httpp "github.com/SkyAPM/go2sky/plugins/http"
 )
+
+func whenErr(w http.ResponseWriter, err error) {
+	fmt.Fprint(w, err.Error())
+	w.WriteHeader(http.StatusInternalServerError)
+}
 
 // HelloWorld writes "Hello, World!" to the HTTP response.
 func HelloWorld(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hello, World!\n")
+	request, err := http.NewRequest("POST", fmt.Sprintf("%s/end", "http://192.168.2.101:8081"), nil)
+	if err != nil {
+		whenErr(w, err)
+		return
+	}
+
+	client, err := httpp.NewClient(go2sky.GetGlobalTracer())
+	if err != nil {
+		whenErr(w, err)
+		return
+	}
+
+	resp, err := client.Do(request)
+	if err != nil {
+		whenErr(w, err)
+		return
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		whenErr(w, err)
+		return
+	}
+
+	fmt.Fprint(w, body)
 }
