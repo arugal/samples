e390265c17bf14c4b2ec70dd7392f85372100802
zhangwei <zhangwei24@apache.org>
2022-03-26 16:07:21 +0800

    test

--- functions/knative/hello-world-go/go.mod
+++ functions/knative/hello-world-go/go.mod
@@ -1,3 +1,8 @@
 module example.com/hello
 
 go 1.16
+
+
+require (
+	"github.com/SkyAPM/go2sky" v1.4.1-0.20220302064553-acee2ee29345
+)
--- functions/knative/hello-world-go/hello.go
+++ functions/knative/hello-world-go/hello.go
@@ -2,9 +2,42 @@ package hello
 
 import (
 	"fmt"
+	"io/ioutil"
 	"net/http"
+
+	"github.com/SkyAPM/go2sky"
+	httpp "github.com/SkyAPM/go2sky/plugins/http"
 )
 
+func whenErr(w http.ResponseWriter, err error) {
+	fmt.Fprint(w, err.Error())
+	w.WriteHeader(http.StatusInternalServerError)
+}
+
 func HelloWorld(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, %s!\n", r.URL.Path[1:])
+	request, err := http.NewRequest("POST", fmt.Sprintf("%s/end", "http://192.168.2.101:8081"), nil)
+	if err != nil {
+		whenErr(w, err)
+		return
+	}
+
+	client, err := httpp.NewClient(go2sky.GetGlobalTracer())
+	if err != nil {
+		whenErr(w, err)
+		return
+	}
+
+	resp, err := client.Do(request)
+	if err != nil {
+		whenErr(w, err)
+		return
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		whenErr(w, err)
+		return
+	}
+
+	fmt.Fprint(w, body)
 }
